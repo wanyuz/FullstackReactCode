2c8cbeafad0ac5eee99b9d2de7a45d6cacf3d443
zwy <zwy@zwys-MacBook-Pro.local>
2020-05-25 20:51:19 -0700

    add auth related stuff

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
-
-const app = express();
-require('./services/passport')
-require('./routes/authRoutes')(app);
 require('./models/User');
+require('./services/passport');
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24* 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 
 mongoose.connect(keys.mongoURI);
 
